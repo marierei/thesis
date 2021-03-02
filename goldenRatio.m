c447ffa2c5b755e7ee60f7845326bfd2e47b1cb4
marierei <38687648+marierei@users.noreply.github.com>
2021-03-02 15:32:27 +0100

    added new chair

---
+++ goldenRatio.m
@@ -0,0 +1,19 @@
+funksjon gold = goldenRatio(x)
+
+% Can I do function inside of function??
+
+
+% Single out the ones needed
+
+% Ratio
+% Compare to ratio
+
+
+global gR;
+
+
+
+
+% Add together the golden ratios
+% Will have 4 for each level = 8 values
+gold = 3;
