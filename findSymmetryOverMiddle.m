3b7b330f249d49afc56c6904f1e93553db6ea08f
Marie Reichelt <marierei@student.matnat.uio.no>
2022-03-10 13:02:55 +0100

    function for symmetry

---
+++ findSymmetryOverMiddle.m
@@ -0,0 +1,13 @@
+function symmetry = findSymmetryOverMiddle(T)
+
+mid = (T(9,:) + T(10,:) + T(11,:) + T(12,:))/4;
+
+sym_1x = abs(abs(T(6,1) - mid(1)) - abs(T(7,1) - mid(1)));
+sym_1y = abs(abs(T(6,2) - mid(2)) - abs(T(7,2) - mid(2)));
+sym_1z = abs(T(6,3) - T(7,3));
+
+sym_2x = abs(abs(T(5,1) - mid(1)) - abs(T(8,1) - mid(1)));
+sym_2y = abs(abs(T(5,2) - mid(2)) - abs(T(8,2) - mid(2)));
+sym_2z = abs(T(5,3) - T(8,3));
+
+symmetry = sym_1x + sym_1y + sym_1z + sym_2x + sym_2y + sym_2z;
